bfb9026e1b5a1a313cd7658caa4adc219b65dcce
jkirtley <joshandrandi@Joshs-MacBook-Pro.local>
2022-09-22 22:50:44 -0600

    change to codeReport.py html gen

--- codeReport.py
+++ codeReport.py
@@ -9,40 +9,84 @@ for student in studentList:
 
 html=f"""
 
-<style>
-table {{
-    border-collapse:separate;
-    border:solid black 1px;
-    border-radius:6px;
-}}
-
-td, th {{
-    border-left:solid #AFAFAF 1px;
-    border-top:solid #AFAFAF 1px;
-    border-color: #AFAFAF;
-
-}}
-
-th {{
-    border-top: none;
-}}
-
-td:first-child, th:first-child {{
-     border-left: none;
-}}
-</style>
-
-<table>
-    <tr>
-        <th>Student</th>
-        <th>Mon</th>
-        <th>Tue</th>
-        <th>Wed</th>
-        <th>Thur</th>
-        <th>Fri</th>
-    </tr>
-        {trs}
-</table>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <table>
+        <tr>
+            <th>Student</th>
+            <th>M</th>
+            <th>T</th>
+            <th>W</th>
+            <th>T</th>
+            <th>F</th>
+        </tr>
+        <!-- THIS IS IT -->
+        <tr>
+          <td>Student 1</td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>Student 2</td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>Student 3</td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>Student 4</td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>Student 5</td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>Student 6</td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>Student 7</td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+        <!-- THIS NEEDS TO CHANGE -->
+    </table>
+  </body>
+</html>
 """
 
 f.write(html)
