dfc23a4d0a99144e1fec5647b0e5f023f03a8374
jkirtley <joshandrandi@Joshs-MacBook-Pro.local>
2022-09-23 14:47:38 -0600

    update using reportlab

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # codeReport
 This is for creating pdf behavior code reports for students
+
+Given a .csv file with course rosters of student names, this automates the entry of students into a printable output html file.
--- codeReport.py
+++ codeReport.py
@@ -1,3 +1,16 @@
+from reportlab.lib import colors
+from reportlab.lib.pagesizes import letter, inch
+from reportlab.platypus import SimpleDocTemplate, Table, TableStyle
+
+# we know some glyphs are missing, suppress warnings
+import reportlab.rl_config
+reportlab.rl_config.warnOnMissingFontGlyphs = 0
+
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
+pdfmetrics.registerFont(TTFont('Nunito-Light', 'Nunito-Light.ttf'))
+
+
 f=open('test.html','w')
 
 studentList=['Josh','Randi','Loosey']
@@ -42,3 +55,23 @@ html=f"""
 
 f.write(html)
 f.close()
+
+doc = SimpleDocTemplate("test.pdf", pagesize=letter)
+elements=[]
+data=  [['Student', 'M', 'T', 'W', 'T', 'F']]
+
+for student in studentList:
+    data.append([f"{student}","","","","",""])
+
+t=Table(data,style=[('GRID',(0,0),(-1,-1),1,colors.grey),
+                    ('GRID',(0,0),(-1,0),1,colors.white),
+                    ('LINEABOVE',(0,0),(-1,1),1,colors.black),
+                    ('BOX',(0,0),(-1,-1),1.5,colors.black),
+                    ('FONTNAME',(0,0),(-1,-1),'Nunito-Light'),
+                    ('BACKGROUND',(0,0),(-1,0), colors.grey),
+                    ('ROUNDEDCORNERS', [2, 2, 2, 2]),
+                    ('TEXTCOLOR',(0,0),(-1,0),colors.white),
+                    ])
+
+elements.append(t)
+doc.build(elements)
