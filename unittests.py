e73defd74bc72048e050bebb2dbb4f750c2c96ec
jkirtley <joshuakirtley1@gmail.com>
2022-09-25 16:31:18 -0600

    updated unittests

--- unittests.py
+++ unittests.py
@@ -1,24 +1,54 @@
 import unittest
-import names
 
 from reportlab.lib.pagesizes import inch
 from reports.tables import StandardTable
 
 class TestStandardTable(unittest.TestCase):
     #This is for creating test classes
-    def students(self,class_size):
-        students=[]
-        while len(students)<class_size:
-            students.append(names.get_full_name())
+    students=['Vernon Magar', 'Laurence Adragna', 'Jami Wright', 'Matthew Lewis', 'Kim Torrez',
+            'Betty Kleinman', 'Frank Gomez', 'Martin Salvato', 'Andy Goodwin', 'Shannon Smyth',
+            'Robert Crouch', 'George Froelich', 'James Brown', 'Michael Vandyke', 'Dorothy Oyler',
+            'Karl Cavanaugh', 'Joan Walls', 'Jerry Willardson', 'Dennis Olson', 'Susan Rodriguez',
+            'Catherine Robinson', 'Hillary Baker', 'Rosa Hoang', 'Gerald Lary', 'Laurie Pree']
 
     def test_columnWidth_function(self):
-        givenInput1=StandardTable(self.students(25),[3,5,4])
+        givenInput1=StandardTable(self.students,[3,5,4])
         expectedOutput1=[3*inch,5*inch,5*inch,5*inch,5*inch,5*inch,4*inch]
         self.assertEqual(givenInput1.columnWidths(), expectedOutput1, f"Should be {expectedOutput1}")
 
-        givenInput2=StandardTable(self.students(29),[1.2,0.5,0.2])
+        givenInput2=StandardTable(self.students,[1.2,0.5,0.2])
         expectedOutput2=[1.2*inch,0.5*inch,0.5*inch,0.5*inch,0.5*inch,0.5*inch,0.2*inch]
         self.assertEqual(givenInput2.columnWidths(), expectedOutput2, f"Should be {expectedOutput2}")
+
+    def test_data_function(self):
+        givenInput1=StandardTable(self.students,[3,5,4])
+        expectedOutput1=[['Student', 'M', 'T', 'W', 'T', 'F','Comments'],
+                        ['Vernon Magar','','','','','',''],
+                        ['Laurence Adragna','','','','','',''],
+                        ['Jami Wright','','','','','',''],
+                        ['Matthew Lewis','','','','','',''],
+                        ['Kim Torrez','','','','','',''],
+                        ['Betty Kleinman','','','','','',''],
+                        ['Frank Gomez','','','','','',''],
+                        ['Martin Salvato','','','','','',''],
+                        ['Andy Goodwin','','','','','',''],
+                        ['Shannon Smyth','','','','','',''],
+                        ['Robert Crouch','','','','','',''],
+                        ['George Froelich','','','','','',''],
+                        ['James Brown','','','','','',''],
+                        ['Michael Vandyke','','','','','',''],
+                        ['Dorothy Oyler','','','','','',''],
+                        ['Karl Cavanaugh','','','','','',''],
+                        ['Joan Walls','','','','','',''],
+                        ['Jerry Willardson','','','','','',''],
+                        ['Dennis Olson','','','','','',''],
+                        ['Susan Rodriguez','','','','','',''],
+                        ['Catherine Robinson','','','','','',''],
+                        ['Hillary Baker','','','','','',''],
+                        ['Rosa Hoang','','','','','',''],
+                        ['Gerald Lary','','','','','',''],
+                        ['Laurie Pree','','','','','','']]
+        self.assertEqual(givenInput1.data(), expectedOutput1, f"Should be {expectedOutput1}")
 
 if __name__=="__main__":
     unittest.main()
