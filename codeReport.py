be64bf87e0fd38eeed39d55a87c39e9f2d14250e
jkirtley <joshuakirtley1@gmail.com>
2022-09-26 21:15:54 -0600

    updated main codeReport.py file

--- codeReport.py
+++ codeReport.py
@@ -1,18 +1,13 @@
 import csv
 from reports.tables import StandardTable
+from student_list.student_list import StudentList
 
 #Inputs
 filepath=input("Path to .csv: ")
 studentColumn=input("Enter the exact name of the column header containing students: ")
 outputFilepath=input("Path to save .pdf: ")
 
-f=open(filepath, newline='')
-reader=csv.DictReader(f)
-
-students=[]
-for row in reader:
-    students.append(row[studentColumn])
-
+students=StudentList(filepath,studentColumn).student_list()
 s=StandardTable(students)
 
 s.createPDF(outputFilepath)
