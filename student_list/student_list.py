3400b0e055c6cefb9b0fc1762fab45c7d5d8a484
jkirtley <joshuakirtley1@gmail.com>
2022-09-26 21:13:33 -0600

    modularized student_list

--- student_list/student_list.py
+++ student_list/student_list.py
@@ -4,3 +4,11 @@ class StudentList:
     def __init__(self,filepath,col_name):
         self.filepath=filepath
         self.col_name=col_name
+
+    def student_list(self):
+        f=open(self.filepath, newline='')
+        reader=csv.DictReader(f)
+        student_list=[]
+        for row in reader:
+            student_list.append(row[self.col_name])
+        return student_list
